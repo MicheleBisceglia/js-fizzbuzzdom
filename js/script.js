1bce1540b25959226e297967bf9be2a68372da90
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-04-11 16:35:08 +0200

    stampa in conosol di frizz,buzz,frizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert("ciao");
+/*Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100.
+Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
+
+for (let i = 1; i <= 100; i++) {
+    let multiple;
+    if (i % 15 === 0) {
+        multiple = "FizzBuzz";
+    }
+
+    else if (i % 3 === 0) {
+        multiple = "Fizz";
+    }
+    
+    else if (i % 5 === 0) {
+        multiple = "Buzz";
+    }
+
+   
+    console.log(i, multiple);
+}
